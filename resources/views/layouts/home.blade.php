aafc2f51faa861639e36cdab3ede28fc949967b2
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2023-04-06 15:02:55 +0200

    test

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -11,7 +11,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
+        <!-- Scriptsd -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased bg-gray-100 bg-center dark:bg-dots-lighter bg-dots-darker dark:bg-gray-900 selection:bg-red-500 selection:text-white">
